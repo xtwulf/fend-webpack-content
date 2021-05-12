e01ab5195a34a679f19182f869e182580486164e
Ulf Wilke <ulf-wilke@web.de>
2021-05-12 21:07:07 +0200

    jnkkn

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import { arsch } from './js/arsch';
 console.log(checkForName);
 
 alert("I EXIST")
